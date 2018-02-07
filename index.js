201d29188b9dd31664a6755660bc9758649adc01
Emil Bay <github@tixz.dk>
2018-02-07 22:53:52 +0100

    Add printable charcode fun

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ app.use(store)
 
 app.route('/', require('./views/main'))
 app.route('/*', require('./views/404'))
+function encodePrintableAscii (byte) {
+  // What about space chars? Look at hexdump
+  if (byte <= 32) return '·'
+  return String.fromCharCode(byte)
+}
 
 if (!module.parent) app.mount('body')
 else module.exports = app

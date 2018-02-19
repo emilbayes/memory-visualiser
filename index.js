fb44bba7f1879a1bb6cd1bff24d752eaf33a69ac
Emil Bay <github@tixz.dk>
2018-02-19 21:38:54 +0100

    Add form for writing memory

--- index.js
+++ index.js
@@ -15,9 +15,38 @@ app.use(funs)
 app.use(table)
 
 app.route('/', function (state, emit) {
-    <input onchange=${update}/>
   return html`<body class="bg-near-black white-80 sans-serif">
+    <form class="pa1 white-80 fr f7" onsubmit=${update}>
+      <section class="measure-narrow">
+        <label for="offset" class="f6 b db mb2">Offset</label>
+        <input  class="input-reset ba b--white-10 pa1 mb1 db w-100" type="numeric" name="offset" placeholder="offset" aria-describedby="offset-desc" />
+        <small id="offset-desc" class="f6 lh-copy white-60 db mb2">
+          Integer for offsetting the bytes in memory
+        </small>
+      </section>
+      <section class="measure-narrow">
+        <label for="offset" class="f6 b db mb2">Data</label>
+        <input class="input-reset ba b--white-10 pa1 mb1 db w-100" type="numeric" name="data" placeholder="data" aria-describedby="data-desc" />
+        <small id="data-desc" class="f6 lh-copy white-60 db mb2">
+          Data to write in memory
+        </small>
+      </section>
+      <section className="measure-narrow">
+        <select name="encoding" class="input-reset ba b--white-10 pa1 mb1 db w-100">
+          <option value="utf8">UTF-8</option>
+          <option value="ascii">ASCII</option>
+          <option value="hex">HEX</option>
+          <option value="base64">base64</option>
+        </select>
+
+        <button type="submit">Apply</button>
+      </section>
+    </form>
+
+    ${funs.render(state, emit)}
+
     <button>Jump to address (i32)</button>
+
     ${table.render(state)}
   </body>`
 

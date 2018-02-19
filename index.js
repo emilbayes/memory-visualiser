884656a6b8be988af20ffaa26c805af1c03f7074
Emil Bay <github@tixz.dk>
2018-02-19 21:38:41 +0100

    Add byte counters and ascii encoding

--- index.js
+++ index.js
@@ -47,23 +47,24 @@ function table (state, emitter) {
 table.render = function (state, emit) {
   var rows = []
 
+  rows.push(html`<thead><th></th>${Array.from(new Array(state.stride), (_, i) => html`<th class="code white-40">${i}</th>`)}<th class="code white-40">ASCII</th></thead>`)
   for (var i = state.offset; i < state.end; i += state.stride) {
-    var cells = []
+    var cells = [html`<th class="code white-40">${i}</th>`]
     for (var j = 0; j < state.stride; j++) {
       cells.push(html`<td class="white-80 code f5 w1 h1">${state.memory[i + j] == null ? '' : state.memory[i + j].toString(16).padStart(2, '0')}</td>`)
     }
 
-    cells.push(html`<code>${Array.from(state.memory.slice(i, i + j), encodePrintableAscii).join('')}</code>`)
+    cells.push(html`<td><code>${Array.from(state.memory.slice(i, i + j), encodePrintableAscii).join('')}</code></td>`)
     rows.push(html`<tr>${cells}</tr>`)
   }
 
   return html`<table>${rows}</table>`
-}
 
-function encodePrintableAscii (byte) {
-  // What about space chars? Look at hexdump
-  if (byte <= 32) return '·'
-  return String.fromCharCode(byte)
+  function encodePrintableAscii (byte) {
+    // What about space chars? Look at hexdump
+    if (byte <= 32 || byte > 127) return '·'
+    return String.fromCharCode(byte)
+  }
 }
 
 if (!module.parent) app.mount('body')

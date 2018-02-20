28ea7767795e930803c8fe8e117e32765c66d033
Emil Bay <github@tixz.dk>
2018-02-20 18:02:37 +0100

    Abstract out memory editor

---
+++ elements/mem.js
@@ -0,0 +1,53 @@
+var html = require('choo/html')
+var css = require('sheetify')
+
+module.exports = mem
+
+var NS = mem.NS = 'mem'
+
+mem.EV_SET = 'mem:set'
+
+function mem (state, emitter) {
+  emitter.on(mem.EV_SET, function (str, encoding, offset) {
+    var buf = Buffer.from(str, encoding)
+
+    state.memory.set(buf, offset)
+    emitter.emit('render')
+  })
+}
+
+mem.render = function (state, emit) {
+  return html`<form class="pa1 white-80 fr f7" onsubmit=${onsubmit}>
+    <section class="measure-narrow">
+      <label for="offset" class="f6 b db mb2">Offset</label>
+      <input  class="input-reset ba b--white-10 pa1 mb1 db w-100" type="numeric" name="offset" placeholder="offset" aria-describedby="offset-desc" />
+      <small id="offset-desc" class="f6 lh-copy white-60 db mb2">
+        Integer for offsetting the bytes in memory
+      </small>
+    </section>
+    <section class="measure-narrow">
+      <label for="offset" class="f6 b db mb2">Data</label>
+      <input class="input-reset ba b--white-10 pa1 mb1 db w-100" type="numeric" name="data" placeholder="data" aria-describedby="data-desc" />
+      <small id="data-desc" class="f6 lh-copy white-60 db mb2">
+        Data to write in memory
+      </small>
+    </section>
+    <section className="measure-narrow">
+      <select name="encoding" class="input-reset ba b--white-10 pa1 mb1 db w-100">
+        <option value="utf8">UTF-8</option>
+        <option value="ascii">ASCII</option>
+        <option value="hex">HEX</option>
+        <option value="base64">base64</option>
+      </select>
+
+      <button type="submit">Apply</button>
+    </section>
+  </form>`
+
+  function onsubmit (e) {
+    e.preventDefault()
+    var d = new FormData(this)
+    emit(mem.EV_SET, d.get('data'), d.get('encoding'), +d.get('offset') || 0)
+    return false
+  }
+}
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var choo = require('choo')
 var html = require('choo/html')
 
 var funs = require('./elements/funcs')
+var mem = require('./elements/mem')
 
 css('tachyons')
 
@@ -12,36 +13,12 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 app.use(funs)
+app.use(mem)
 app.use(table)
 
 app.route('/', function (state, emit) {
   return html`<body class="bg-near-black white-80 sans-serif">
-    <form class="pa1 white-80 fr f7" onsubmit=${update}>
-      <section class="measure-narrow">
-        <label for="offset" class="f6 b db mb2">Offset</label>
-        <input  class="input-reset ba b--white-10 pa1 mb1 db w-100" type="numeric" name="offset" placeholder="offset" aria-describedby="offset-desc" />
-        <small id="offset-desc" class="f6 lh-copy white-60 db mb2">
-          Integer for offsetting the bytes in memory
-        </small>
-      </section>
-      <section class="measure-narrow">
-        <label for="offset" class="f6 b db mb2">Data</label>
-        <input class="input-reset ba b--white-10 pa1 mb1 db w-100" type="numeric" name="data" placeholder="data" aria-describedby="data-desc" />
-        <small id="data-desc" class="f6 lh-copy white-60 db mb2">
-          Data to write in memory
-        </small>
-      </section>
-      <section className="measure-narrow">
-        <select name="encoding" class="input-reset ba b--white-10 pa1 mb1 db w-100">
-          <option value="utf8">UTF-8</option>
-          <option value="ascii">ASCII</option>
-          <option value="hex">HEX</option>
-          <option value="base64">base64</option>
-        </select>
-
-        <button type="submit">Apply</button>
-      </section>
-    </form>
+    ${mem.render(state, emit)}
 
     ${funs.render(state, emit)}
 
@@ -49,16 +26,6 @@ app.route('/', function (state, emit) {
 
     ${table.render(state)}
   </body>`
-
-  function update (e) {
-    e.preventDefault()
-    var d = new FormData(this)
-    emit('data:update', {
-      offset: +d.get('offset') || 0,
-      data: Buffer.from(d.get('data'), d.get('encoding'))
-    })
-    return false
-  }
 })
 
 function table (state, emitter) {

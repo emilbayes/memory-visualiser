7d4a03cc6a89627ae3cd134e38f1278d1c94df50
Emil Bay <github@tixz.dk>
2018-02-20 18:07:24 +0100

    Set default memory offset in input

--- elements/mem.js
+++ elements/mem.js
@@ -20,7 +20,7 @@ mem.render = function (state, emit) {
   return html`<form class="pa1 white-80 fr f7" onsubmit=${onsubmit}>
     <section class="measure-narrow">
       <label for="offset" class="f6 b db mb2">Offset</label>
-      <input  class="input-reset ba b--white-10 pa1 mb1 db w-100" type="numeric" name="offset" placeholder="offset" aria-describedby="offset-desc" />
+      <input  class="input-reset ba b--white-10 pa1 mb1 db w-100" type="numeric" name="offset" placeholder="offset" aria-describedby="offset-desc" value=0 />
       <small id="offset-desc" class="f6 lh-copy white-60 db mb2">
         Integer for offsetting the bytes in memory
       </small>
@@ -47,7 +47,7 @@ mem.render = function (state, emit) {
   function onsubmit (e) {
     e.preventDefault()
     var d = new FormData(this)
-    emit(mem.EV_SET, d.get('data'), d.get('encoding'), +d.get('offset') || 0)
+    emit(mem.EV_SET, d.get('data'), d.get('encoding'), +d.get('offset'))
     return false
   }
 }

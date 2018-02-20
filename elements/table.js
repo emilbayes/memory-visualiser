92b0b7cf441e613e4bbdfd2541d311b956733e28
Emil Bay <github@tixz.dk>
2018-02-20 18:07:34 +0100

    Abstract out table element

---
+++ elements/table.js
@@ -0,0 +1,39 @@
+var html = require('choo/html')
+var css = require('sheetify')
+
+module.exports = table
+
+var NS = table.NS = 'table'
+
+function table (state, emitter) {
+  var s = state[NS] = {}
+  s.stride = 32
+  s.offset = 0
+  s.end = 256
+}
+
+table.render = function (state, emit) {
+  var s = state[NS]
+
+  var rows = []
+
+  rows.push(html`<thead><th></th>${Array.from(new Array(s.stride), (_, i) => html`<th class="code white-40">${i}</th>`)}<th class="code white-40">ASCII</th></thead>`)
+  for (var i = s.offset; i < s.end; i += s.stride) {
+    var cells = [html`<th class="code white-40">${i}</th>`]
+    for (var j = 0; j < s.stride; j++) {
+      var val = state.memory[i + j] == null ? '' : state.memory[i + j].toString(16).padStart(2, '0')
+      cells.push(html`<td class="white-${val !== '00' ? '80' : '50'} code f5 w1 h1">${val}</td>`)
+    }
+
+    cells.push(html`<td><code>${Array.from(state.memory.slice(i, i + j), encodePrintableAscii).join('')}</code></td>`)
+    rows.push(html`<tr>${cells}</tr>`)
+  }
+
+  return html`<table>${rows}</table>`
+
+  function encodePrintableAscii (byte) {
+    // What about space chars? Look at hexdump
+    if (byte <= 32 || byte > 127) return '·'
+    return String.fromCharCode(byte)
+  }
+}
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var html = require('choo/html')
 
 var funs = require('./elements/funcs')
 var mem = require('./elements/mem')
+var table = require('./elements/table')
 
 css('tachyons')
 
@@ -27,42 +28,6 @@ app.route('/', function (state, emit) {
     ${table.render(state)}
   </body>`
 })
-
-function table (state, emitter) {
-  state.stride = 32
-  state.offset = 0
-  state.end = 1024
-
-  emitter.on('data:update', function (d) {
-    state.memory.set(d.data, d.offset)
-
-    emitter.emit('render')
-  })
-}
-
-table.render = function (state, emit) {
-  var rows = []
-
-  rows.push(html`<thead><th></th>${Array.from(new Array(state.stride), (_, i) => html`<th class="code white-40">${i}</th>`)}<th class="code white-40">ASCII</th></thead>`)
-  for (var i = state.offset; i < state.end; i += state.stride) {
-    var cells = [html`<th class="code white-40">${i}</th>`]
-    for (var j = 0; j < state.stride; j++) {
-      var val = state.memory[i + j] == null ? '' : state.memory[i + j].toString(16).padStart(2, '0')
-      cells.push(html`<td class="white-${val !== '00' ? '80' : '50'} code f5 w1 h1">${val}</td>`)
-    }
-
-    cells.push(html`<td><code>${Array.from(state.memory.slice(i, i + j), encodePrintableAscii).join('')}</code></td>`)
-    rows.push(html`<tr>${cells}</tr>`)
-  }
-
-  return html`<table>${rows}</table>`
-
-  function encodePrintableAscii (byte) {
-    // What about space chars? Look at hexdump
-    if (byte <= 32 || byte > 127) return '·'
-    return String.fromCharCode(byte)
-  }
-}
 
 app.use(function init (state, emitter) {
   emitter.emit(funs.EV_SET_MODULE, require('./reverse-wasm')())
